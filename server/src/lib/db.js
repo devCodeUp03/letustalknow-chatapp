9bd74b19bf188ae38e52d5ec8d383996d0e5b00e
Devashish Upreti <devashup68@gmail.com>
2025-06-10 09:11:28 +0545

    increased timeou

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error: ", error);
   }
-  
 };
