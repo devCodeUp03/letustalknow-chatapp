3b63a0227ecdcacde31d1b39310db3a01b5fe5ba
Devashish Upreti <devashup68@gmail.com>
2025-06-10 08:47:45 +0545

    port

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ import { app, server } from "./lib/socket.js";
 import path from "path";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
