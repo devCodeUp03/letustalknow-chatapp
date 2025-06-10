7e1792ec478f104372c8c5425f8d33030bbcbb8b
Devashish Upreti <devashup68@gmail.com>
2025-06-10 08:07:02 +0545

    path configuration

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,7 @@ app.use("/api/messages", messageRoutes);
 //integration frontend and backend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*any", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
