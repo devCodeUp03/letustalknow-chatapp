cb45c110280a029414cde8309cf0c956212e5987
Devashish Upreti <devashup68@gmail.com>
2025-06-10 09:34:36 +0545

    optional object

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -3,15 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-      serverSelectionTimeoutMS: 30000, // 30 seconds
-      socketTimeoutMS: 45000,
-      maxPoolSize: 20, // Increased pool size
-      minPoolSize: 5, // Maintain minimum connections
-      connectTimeoutMS: 30000, // Separate connect timeout
-      waitQueueTimeoutMS: 20000, // Wait time for connection from pool
-      heartbeatFrequencyMS: 3000, // More frequent health checks
-      retryWrites: true,
-      retryReads: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
